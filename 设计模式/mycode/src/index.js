1777c0f7a904cd77518e367e09946edd10637f23
郭瑞鹏 <guoruipeng@guoruipengdeMacBook-Pro-2.local>
2019-10-28 00:19:41 +0800

    工厂，单例，适配器，装饰器模式

--- 设计模式/mycode/src/index.js
+++ 设计模式/mycode/src/index.js
@@ -1,35 +1,10 @@
-class Trip {
-  constructor(car) {
-      this.car = car
-  }
-  start() {
-      console.log(`行程开始，名称: ${this.car.name}, 车牌号: ${this.car.number}`)
-  }
-  end(distance = 0) {
-      console.log('行程结束，价格: ' + (this.car.price * distance))
-  }
+// 一个简单的 demo
+@testDec
+class Demo {
+  // ...
 }
 
-class Car {
-  constructor(number, name, price, type) {
-      this.number = number
-      this.name = name
-      this.price = price
-      this.type = type
-  }
+function testDec(target) {
+  target.isDec = true;
 }
-
-function carFactory(number, name, type) {
-  let price
-  if(type === '快车') {
-    price = 1
-  } else if (type === '专车') {
-    price = 2
-  }
-  return new Car(number, name, price, type)
-}
-
-let car = carFactory(100, '桑塔纳', '快车')
-let trip = new Trip(car)
-trip.start()
-trip.end(5)
+alert(Demo.isDec) // true
--- 设计模式/mycode/webpack.config.js
+++ 设计模式/mycode/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devServer: {
     contentBase: './dist',
     open: true,
-    port: 9000,
+    port: 9001,
     hot: true,
     // hotOnly: true
   },
